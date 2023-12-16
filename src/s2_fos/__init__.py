cc60316eeea14e384d0d590dc39d76fee05c6a7d
Egor Klevak <egork@allenai.org>
2023-12-15 17:17:51 -0800

    Ended up moving FOS_LIST to constants.py, otherwise was getting into circular dependencies

--- src/s2_fos/__init__.py
+++ src/s2_fos/__init__.py
@@ -1,29 +1,6 @@
-from s2_fos.model import PredictProbabilities
-
-FOS_LIST = [
-    'Agricultural and Food Sciences',
-    'Art',
-    'Biology',
-    'Business',
-    'Chemistry',
-    'Computer Science',
-    'Economics',
-    'Education',
-    'Engineering',
-    'Environmental Science',
-    'Geography',
-    'Geology',
-    'History',
-    'Law',
-    'Linguistics',
-    'Materials Science',
-    'Mathematics',
-    'Medicine',
-    'Philosophy',
-    'Physics',
-    'Political Science',
-    'Psychology',
-    'Sociology']
+from s2_fos.interface import S2FOS
+from s2_fos.constants import LABELS
 
 
-__all__ = ['PredictProbabilities']
+
+__all__ = ['LABELS', 'S2FOS']
