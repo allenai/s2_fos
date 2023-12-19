82d8dc438d61925b20bf889f9d5412179e645374
Egor Klevak <egork@allenai.org>
2023-12-18 17:12:36 -0800

    Adding working version of open_ai_prompts.py

--- src/s2_fos/training/open_ai_prompts.py
+++ src/s2_fos/training/open_ai_prompts.py
@@ -1,209 +1,115 @@
+import sys
+from pathlib import Path
+
+# Add the src directory to sys.path
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent))
+
+import json
+
 import numpy as np
-
-from s2_fos.src import FOS_LIST
-FOS_LIST = [
-    "Agricultural and Food sciences",
-    "Art",
-    "Biology",
-    "Business",
-    "Chemistry",
-    "Computer science",
-    "Economics",
-    "Education",
-    "Engineering",
-    "Environmental science",
-    "Geography",
-    "Geology",
-    "History",
-    "Law",
-    "Linguistics",
-    "Materials science",
-    "Mathematics",
-    "Medicine",
-    "Philosophy",
-    "Physics",
-    "Political science",
-    "Psychology",
-    "Sociology",
-    'Other'
-]
-
-
-def search_for_fos_respons(response):
-    fields = []
-    for field in FOS_LIST:
-        if field in response:
-            fields.append(field)
-
-    return fields
-
-
-def structure_open_ai_response(open_ai_calls):
-    open_ai_list = []
-    for key, value in open_ai_calls.items():
-        paper_sha = key[:40]
-        model = key[41:]
-        # print(paper_sha, model)
-        extracted_fields = search_for_fos_respons(value['result']['choices'][0]['message']['content'])
-        completion_tokens = value['result']['usage']['completion_tokens']
-        prompt_tokens = value['result']['usage']['prompt_tokens']
-        open_ai_list.append({'paper_sha': paper_sha, 'model': model,
-                             'FoS_openAI': extracted_fields, 'completion_tokens': completion_tokens,
-                             'prompt_tokens': prompt_tokens})
-
-    return open_ai_list
-
-
-from pyspark.sql.types import StructType, StructField, StringType, ArrayType, IntegerType
-
-
-def create_spark_df_open_ai(open_ai_calls):
-    open_ai_calls_dict_list = structure_open_ai_response(open_ai_calls)
-    schema = StructType([
-        StructField("paper_sha", StringType(), True),
-        StructField("model", StringType(), True),
-        StructField("FoS_openAI", ArrayType(StringType()), True),
-        StructField("completion_tokens", IntegerType(), True),
-        StructField("prompt_tokens", IntegerType(), True),
-    ])
-    return spark.createDataFrame(open_ai_calls_dict_list, schema=schema)
-
-
 import re
+import openai
+import time
+import os
+from s2_fos.constants import LABELS, PROJECT_ROOT_PATH
+
+# Define constants
+MODEL = "gpt-3.5-turbo"
+MAX_RETRIES = 5
+RETRY_DELAY = 5  # Time in seconds to wait before retrying
+
+# Check and Set OpenAI API key
+if 'OPENAI_API_KEY' in os.environ:
+    openai.api_key = os.environ['OPENAI_API_KEY']
+else:
+    raise Warning("OPENAI_API_KEY not found in environment variables. Please set it before running this script.")
 
 def tokenize(text):
-    if text:
-        return re.findall(r'\b\w+\b', text)
-    return text
+    return re.findall(r'\b\w+\b', text) if text else []
 
 def truncate_entity(entity, max_tokens=800):
     tokens = tokenize(entity)
-    if tokens and len(tokens) > max_tokens:
-        tokens = tokens[:max_tokens]
-        truncated_entity = ' '.join(tokens)
-        return truncated_entity
-    else:
-        return entity
-
-
-import openai
-import time
-import json
-
-model = "gpt-3.5-turbo"
-max_retries = 5
-retry_delay = 5  # Time in seconds to wait before retrying
-
+    return ' '.join(tokens[:max_tokens]) if tokens and len(tokens) > max_tokens else entity
 
 def call_openai_api(model, messages, temperature=0):
     return openai.ChatCompletion.create(model=model, messages=messages, temperature=temperature)
 
+def search_for_fos_response(response):
+    return [field for field in LABELS if field in response]
+
+def prompt_with_journal(title, abstract, journal_name):
+    fos_string = "', '".join(LABELS)
+    return [
+        {"role": "system", "content": "You are a highly intelligent and accurate information extraction system. "
+                                      "You take title, abstract, journal name of a scientific article as input and your"
+                                      " task is to classify the scientific field of study of the passage."},
+        {"role": "user", "content": f"You need to classify it with key: 'field_of_study' assign as many "
+                                    f"'field_of_study' as you find it fit: '{fos_string}'. Only select from the above "
+                                    f"list, or 'Other'."},
+        {"role": "assistant", "content": f"```python\ntitle = {title}\nabstract = {abstract}\n"
+                                         f"journal_name = {journal_name}\nfield_of_study = []"}
+    ]
 
 def process_paper(paper_sha, title, abstract, journal_name, model, open_ai_calls):
     key = f'{paper_sha}_{model}'
-    if key not in open_ai_calls.keys():
-        if idx % 100 == 0:
-            print(idx, paper_sha)
-
+    if key not in open_ai_calls:
         truncated_title = truncate_entity(title)
         truncated_abstract = truncate_entity(abstract)
         messages = prompt_with_journal(truncated_title, truncated_abstract, journal_name)
 
-        retries = 0
-        success = False
-        while not success and retries < max_retries:
+        for _ in range(MAX_RETRIES):
             try:
                 result = call_openai_api(model, messages, temperature=0)
-                if len(search_for_fos_respons(result['choices'][0]['message']['content'])) == 0:
-                    # Extract the assistant's reply
-                    assistant_reply1 = result['choices'][0]['message']['content']
-
-                    # Add the user's follow-up question
-                    assistant_reply1 = assistant_reply1.replace("```", "``")
-                    # Format the content string
-                    content_string = f"Can you please map your response {assistant_reply1} to the following fields: {', '.join(FOS_LIST)}"
+                if not search_for_fos_response(result['choices'][0]['message']['content']):
+                    assistant_reply1 = result['choices'][0]['message']['content'].replace("```", "``")
+                    content_string = (f"Can you please map your response {assistant_reply1} to the following fields: "
+                                      f"{', '.join(LABELS)}")
                     messages.extend([
                         {"role": "user", "content": content_string},
-                        {"role": "assistant",
-                         "content": ("```python \n"
-                                     "{'field_of_study': [")}, ])
-
+                        {"role": "assistant", "content": "```python \n{'field_of_study': ["}
+                    ])
                     result = call_openai_api(model, messages, temperature=0)
-                    open_ai_calls[key] = {'prompt': messages, 'result': result}
-                else:
-                    open_ai_calls[key] = {'prompt': messages, 'result': result}
-                success = True
+                open_ai_calls[key] = {'prompt': messages, 'result': result}
+                break
             except openai.OpenAIError as e:
                 print(f"Error: {e}")
-                if messages:
-                    print(f'Length of the message: {len(tokenize(json.dumps(messages)))}')
-                if truncated_title and truncated_abstract:
-                    print(
-                        f'Length of title: {len(tokenize(truncated_title))} Length of Abstract: {len(tokenize(truncated_abstract))}')
-                print(
-                    f'Paper_sha: {paper_sha} Title: {truncated_title}\n Abstract: {truncated_abstract}\n Journal Name: {journal_name}')
-                if retries < max_retries:
-                    print(f"Retrying in {retry_delay} seconds...")
-                    time.sleep(retry_delay)
-                    retries += 1
-                else:
-                    print("Max retries reached. Skipping this API call.")
+                time.sleep(RETRY_DELAY)
+        else:
+            print("Max retries reached. Skipping this API call.")
     return open_ai_calls
 
+def save_openai_calls(open_ai_calls, filename):
+    with open(filename, 'w') as file_handle:
+        json.dump(open_ai_calls, file_handle)
 
-def prompt_with_journal(title, abstract, journal_name):
-    message = [
-        {"role": "system", "content": "You are a highly intelligent and accurate information extraction system. You take title, abstract, journal name of a \
-         scientific article as input and your task is to classify the scientific field of study of the passage.",
-         "role": "user", "content": "You need to classify it with key: 'field_of_study' assign as many 'field_of_study' as you find it fit: \
-    'Agricultural and Food sciences', \
-    'Art', \
-    'Biology',\
-    'Business',\
-    'Chemistry',\
-    'Computer science',\
-    'Economics',\
-    'Education',\
-    'Engineering',\
-    'Environmental science',\
-    'Geography',\
-    'Geology',\
-    'History',\
-    'Law',\
-    'Linguistics',\
-    'Materials science',\
-    'Mathematics',\
-    'Medicine',\
-    'Philosophy',\
-    'Physics',\
-    'Political science',\
-    'Psychology',\
-    'Sociology'\
-    Only select from the above list, or 'Other'."},
+def main():
+    # Path to your .jsonl file
+    jsonl_file_path = os.path.join(PROJECT_ROOT_PATH, '..', 'data',
+                                   'paper_title_abstract_example_file.json')
 
-        {"role": "assistant",
-         "content": ("```python \n"
-                     f"title = {title} \n"
-                     f"abstract = {abstract}\n"
-                     f"journal_name = {journal_name}\n"
-                     "{'field_of_study': ["
-                     )}, ]
-    return message
+    # Initialize an empty list to store the JSON objects
+    json_list = []
 
+    # Open the JSONL file and read each line
+    with open(jsonl_file_path, 'r') as file:
+        for line in file:
+            # Parse the JSON string into a Python dictionary
+            json_obj = json.loads(line.strip())
+            # Append the dictionary to the list
+            json_list.append(json_obj.values())
 
-idx = 0
-model = "gpt-3.5-turbo"
-max_retries = 5
-retry_delay = 2  # Time in seconds to wait before retrying
+    # Loading jsonl file
+    meta_data_np = np.array(json_list)  # Replace with actual data
+    open_ai_calls = {}
+    for idx, (paper_sha, title, abstract, journal_name) in enumerate(meta_data_np):
+        open_ai_calls = process_paper(paper_sha, title, abstract, journal_name, MODEL, open_ai_calls)
+        if (idx + 1) % 100 == 0:
+            save_openai_calls(open_ai_calls, os.path.join(PROJECT_ROOT_PATH, '..', 'data',
+                                                          f'open_ai_most_popular_{idx + 1}.json'))
 
-meta_data_np = np.array()
-for paper_sha, title, abstract, journal_name in meta_data_np:
+    save_openai_calls(open_ai_calls,
+                      os.path.join(PROJECT_ROOT_PATH, '..', 'data',
+                                   'open_ai_most_popular_final.json'))
 
-    open_ai_calls = process_paper(paper_sha, title, abstract, journal_name, model, open_ai_calls)
-    idx += 1
-    if idx % 100 == 0:
-        with open(f'../upsample_under_perform_s2_fos_2/open_ai_most_popular_{idx}.pkl', 'wb') as file_handle:
-            pickle.dump(open_ai_calls, file_handle)
-
-with open(f'../upsample_under_perform_s2_fos_2/open_ai_most_popular_final.pkl', 'wb') as file_handle:
-    pickle.dump(open_ai_calls, file_handle)
+if __name__ == "__main__":
+    main()
