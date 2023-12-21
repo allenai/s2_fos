f76b37ae18217da7b238c03d746a3d24b4791b85
Egor Klevak <egork@allenai.org>
2023-12-20 16:17:19 -0800

    Updating parameter

--- src/s2_fos/training/run.sh
+++ src/s2_fos/training/run.sh
@@ -2,5 +2,5 @@
 # Training parameters and Asymetric loss function parameters were obtained through running ablation experiments
 python train_net.py --text_fields title abstract journal_name  --save_path /stage/output/ \
 --train True --model_checkpoint_path /stage/output/ --project_name <weights and biases project name> \
---batch_size 32 --learning_rate 1e-5 --warmup_ratio 0.06 --wandb_name <run name> --wandb_run_des <run description> \
+--batch_size 32 --learning_rate 1e-5 --warmup_ratio 0.06 --wandb_name <run name> --wandb_run_desc <run description> \
  --log_dir /output --loss Asymmetric --asym_gamma_neg 4 --asym_gamma_pos 2 --asym_clip 0.1
