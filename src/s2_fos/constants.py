9ed0a96cf936cca1c7a077290680be6947bb981f
Egor Klevak <egork@allenai.org>
2023-12-23 17:59:28 -0800

    Revert "Applying black autoformatting tool"
    This reverts commit 96e5ae6e4821adb75f05bdab91c81ca6ae2ad25a.

--- src/s2_fos/constants.py
+++ src/s2_fos/constants.py
@@ -1,38 +1,38 @@
 import os
 
-FASTTEXT_FNAME = "lid.176.bin"
+FASTTEXT_FNAME = 'lid.176.bin'
 
-MODEL_NAME = "allenai/scibert_scivocab_uncased_field_of_study"
+MODEL_NAME = 'allenai/scibert_scivocab_uncased_field_of_study'
 
 LABELS = [
-    "Agricultural and Food Sciences",
-    "Art",
-    "Biology",
-    "Business",
-    "Chemistry",
-    "Computer Science",
-    "Economics",
-    "Education",
-    "Engineering",
-    "Environmental Science",
-    "Geography",
-    "Geology",
-    "History",
-    "Law",
-    "Linguistics",
-    "Materials Science",
-    "Mathematics",
-    "Medicine",
-    "Philosophy",
-    "Physics",
-    "Political Science",
-    "Psychology",
-    "Sociology",
-]
+    'Agricultural and Food Sciences',
+    'Art',
+    'Biology',
+    'Business',
+    'Chemistry',
+    'Computer Science',
+    'Economics',
+    'Education',
+    'Engineering',
+    'Environmental Science',
+    'Geography',
+    'Geology',
+    'History',
+    'Law',
+    'Linguistics',
+    'Materials Science',
+    'Mathematics',
+    'Medicine',
+    'Philosophy',
+    'Physics',
+    'Political Science',
+    'Psychology',
+    'Sociology']
 
 try:
-    PROJECT_ROOT_PATH = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir))
+    PROJECT_ROOT_PATH = os.path.abspath(
+        os.path.join(__file__, os.pardir, os.pardir))
 except NameError:
     PROJECT_ROOT_PATH = os.path.abspath(os.path.join(os.getcwd()))
 
-TOKENIZER_MODEL_NAME = "allenai/scibert_scivocab_uncased"
+TOKENIZER_MODEL_NAME = 'allenai/scibert_scivocab_uncased'
--- src/s2_language_detection/language_classifier.py
+++ src/s2_language_detection/language_classifier.py
@@ -12,17 +12,14 @@ class LanguageClassifier:
     def __init__(self, data_dir: str = None):
         self.logger = logging.getLogger(__name__)
         if data_dir is None:
-            self.model_path = os.path.join(
-                PROJECT_ROOT_PATH, os.pardir, "data", FASTTEXT_FNAME
-            )
+            self.model_path = os.path.join(PROJECT_ROOT_PATH, os.pardir, 'data', FASTTEXT_FNAME)
         else:
             self.model_path = os.path.join(data_dir, FASTTEXT_FNAME)
         if not os.path.exists(self.model_path):
             raise FileNotFoundError(
                 f"Language classifier model {FASTTEXT_FNAME} not found in {PROJECT_ROOT_PATH} \n"
                 f"Please download the model from https://fasttext.cc/docs/en/language-identification.html"
-                f"and place it in {data_dir}"
-            )
+                f"and place it in {data_dir}")
 
         self.fasttext_model = fasttext.load_model(self.model_path)
 
@@ -37,9 +34,7 @@ class LanguageClassifier:
             result.append(self.detect_language(*text_title_abstract))
         return result
 
-    def detect_language(
-        self, text_title: str = None, text_abstract: str = None
-    ) -> Union[str, float]:
+    def detect_language(self, text_title: str = None, text_abstract: str = None) -> Union[str, float]:
         """
         Detect the language used in the input text with trained language classifier. If abstract is present, and it does
         not contain 'no abstract' returns language of the abstract and the soft max_value prediction. Returns
@@ -47,13 +42,10 @@ class LanguageClassifier:
         :param text_title Title of the paper
         :param text_abstract Abstract of the paper
         """
-        if text_abstract and "no abstract" not in text_abstract:
-            (
-                predicted_language_abstract,
-                soft_max_pred_abstract,
-            ) = self.detect_language_single_entity(text_abstract)
+        if text_abstract and 'no abstract' not in text_abstract:
+            predicted_language_abstract, soft_max_pred_abstract = self.detect_language_single_entity(text_abstract)
 
-            if predicted_language_abstract != "un":
+            if predicted_language_abstract != 'un':
                 return predicted_language_abstract, soft_max_pred_abstract
 
         return self.detect_language_single_entity(text_title)
@@ -65,15 +57,15 @@ class LanguageClassifier:
         :param text to predict language for
         """
         if not text:
-            return "un", 0.0
+            return 'un', 0.0
 
-        fasttext_pred = self.fasttext_model.predict(text.lower().replace("\n", " "))
-        predicted_language_ft = fasttext_pred[0][0].split("__")[-1]
+        fasttext_pred = self.fasttext_model.predict(text.lower().replace('\n', ' '))
+        predicted_language_ft = fasttext_pred[0][0].split('__')[-1]
         soft_max_pred = fasttext_pred[1][0]
 
         # Chinese and japanese do not use white space for word separation, thus we first check if the predicted language
         # is not one from the list before checking how many words the text contains.
-        if predicted_language_ft not in ["zh", "jp"] and len(text.split()) <= 1:
-            return "un", 0.0
+        if predicted_language_ft not in ['zh', 'jp'] and len(text.split()) <= 1:
+            return 'un', 0.0
 
         return predicted_language_ft, soft_max_pred
